c3e352ece370bfc7794b469c184729c9d6ad8111
Mateus Pinna Lousa <mateuslousa@gmail.com>
2023-01-07 00:22:27 -0300

    Falta apenas o export sozinho

--- src/export.c
+++ src/export.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpinna-l <mpinna-l@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/06 22:07:25 by mpinna-l          #+#    #+#             */
+/*   Updated: 2023/01/07 00:21:07 by mpinna-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-int valid_variable (char *c)
+int	valid_variable(char *c)
 {
 	int	i;
 
@@ -8,36 +20,21 @@ int valid_variable (char *c)
 	while (c[++i])
 	{
 		if (c[i] == '=' && i == 0)
-			return (0) ;
-		else if (c[i] == '=' && i != 1)
+			return (0);
+		else if (c[i] == '=' && i != 0)
 			break ;
 		else if ((!ft_isalpha(c[i]) && !(c[i] == '_')) && i == 0)
-        	return (0);
-    	else if ((!ft_isalpha(c[i]) && !ft_isdigit(c[i]) && !(c[i] == '_')))
-        	return (0);
+			return (0);
+		else if ((!ft_isalpha(c[i]) && !ft_isdigit(c[i]) && !(c[i] == '_')))
+			return (0);
 	}
 	return (1);
 }
 
-void	search_and_replace(char *variable, t_env *env, char *value)
+void	replace(int i, t_env *env, char *variable, char *value)
 {
-    int     i;
-    int     len;
-	char	*temp;
 	char	**new_env;
 
-    i = -1;
-    len = ft_strlen(variable);
-	while (env->env[++i])
-    {
-        if (!ft_strncmp(variable, env->env[i], len))
-        {
-			temp = env->env[i];
-			env->env[i] = ft_strjoin(variable, value);
-			free(temp);
-            return ;
-        }
-    }
 	new_env = malloc(sizeof(char *) * (i + 2));
 	i = 0;
 	while (env->env[i])
@@ -50,14 +47,51 @@ void	search_and_replace(char *variable, t_env *env, char *value)
 	new_env[i++] = ft_strjoin(variable, value);
 	new_env[i] = NULL;
 	env->env = new_env;
-    return ;
+	return ;
 }
 
-int ft_export(char **args, t_env *env)
+void	search_and_replace(char *variable, t_env *env, char *value)
+{
+	int		i;
+	int		len;
+	char	*temp;
+
+	i = -1;
+	len = ft_strlen(variable);
+	while (env->env[++i])
+	{
+		if (!ft_strncmp(variable, env->env[i], len))
+		{
+			temp = env->env[i];
+			env->env[i] = ft_strjoin(variable, value);
+			free(temp);
+			return ;
+		}
+	}
+	replace(i, env, variable, value);
+}
+
+void	extract(t_env *env, char *args, int flag)
 {
 	int		i;
 	char	*variable;
 	char	*value;
+
+	i = 0;
+	while (args[i] != '=')
+		i++;
+	variable = ft_substr(args, 0, i + 1);
+	value = ft_strdup(ft_strchr(args, '=') + 1);
+	if (flag == 0)
+		search_and_replace(variable, env, value);
+	if (flag == 1)
+		printf("Minishell: export: `%s': not a valid identifier\n", variable);
+	free(variable);
+	free(value);
+}
+
+int	ft_export(char **args, t_env *env)
+{
 	int		j;
 
 	j = 0;
@@ -66,27 +100,14 @@ int ft_export(char **args, t_env *env)
 	while (args[++j])
 	{
 		if (valid_variable(args[j]) && (ft_strchr(args[j], '=')))
-		{
-			i = 0;
-			while (args[j][i++] != '=');
-			variable = ft_substr(args[j], 0, i);
-			value = ft_strdup(ft_strchr(args[j], '=') + 1);
-			search_and_replace(variable, env, value);
-			free(variable);
-			free(value);
-		}
+			extract(env, args[j], 0);
 		else
 		{
 			if (ft_strchr(args[j], '='))
-			{
-				i = 0;
-				while (args[j][i++] != '=');
-				variable = ft_substr(args[j], 0, i);
-				printf("Minishell: export: `%s': not a valid identifier\n", variable);
-				free(variable);
-			}
+				extract(env, args[j], 1);
 			else
-				printf("Minishell: export: `%s': not a valid identifier\n", args[j]);
+				printf("Minishell: export: `%s':"
+					" not a valid identifier\n", args[j]);
 		}
 	}
 	return (0);
